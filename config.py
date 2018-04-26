9a094c2634a9be0f3b95d21186c505ea82a50df9
Jurgen <jurgen.geitner@gmail.com>
2018-04-26 15:29:53 +0200

    Configuration changes

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.sendgrid.net')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SLIDEDB_MAIL_SUBJECT_PREFIX = '[UCT PathSlides]'
-    SLIDEDB_MAIL_SENDER = 'UCT PathSlides Admin <jurgen.geitner@gmail.com>'
+    SLIDEDB_MAIL_SENDER = 'UCT PathSlides Admin <somebody@example.com>'
     SLIDEDB_ADMIN = os.environ.get('SLIDEDB_ADMIN')
     SLIDEDB_PROF = os.environ.get('SLIDEDB_PROF')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
